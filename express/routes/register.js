3fbf4805f99833a92ef46f1e4f16cfe3db46ae06
Najiya-web <najiyashaikh961@gmail.com>
2026-05-22 08:50:39 +0530

    fixed bug

--- express/routes/register.js
+++ express/routes/register.js
@@ -2,4 +2,4 @@ const express=require('express');
 const register=require('../controllers/Register');
 const route=express.Router();
 route.post('/register',register);
-module.exports=register;
+module.exports=route;
